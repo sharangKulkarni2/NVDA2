5842162ddd5ac8eab2c744df557af692ddfbcfea
sharang kulkarni <45118333+sharangKulkarni2@users.noreply.github.com>
2020-11-25 11:47:51 +0530

    Add files via upload

---
+++ addon/globalPlugins/modules/package/demo.py
@@ -0,0 +1,7 @@
+def print_to_ten():
+    print('hello')
+    for i in range(10):
+        print(i)
+
+if __name__ == '__main__':
+    print_to_ten()
---
+++ addon/globalPlugins/modules/package/demod.py
@@ -0,0 +1,8 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+import demo
+
+
+def demod_print_to_ten():
+    demo.print_to_ten()
